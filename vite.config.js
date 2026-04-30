db55cf17e52e3e1fa4e2c2836c00dadd619a69e0
Sebastián Feuerhake <sebastianfl@gmail.com>
2026-04-30 17:03:20 -0400

    Correccion de rutas absolutas e icono PNG

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['logo.png'], // Asegura que copie el logo
       manifest: {
         name: 'CiruReg AI',
         short_name: 'CiruReg',
